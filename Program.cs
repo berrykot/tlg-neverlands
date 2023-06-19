52d1dff4059870323b80a2db61b81bc1a4ab0ddd
berrykot <berrykot@gmail.com>
2023-06-19 11:43:01 +0300

    Настройка сервиса работать на 2х портах и gRPC рефлексии 2 порта нужно тольео для DEV т.к. локально работаем без SSL, который позволяет определить вид трафика (http1 или http2)

--- Program.cs
+++ Program.cs
@@ -4,11 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc().AddJsonTranscoding();
+//При запросе gRPC в т.ч. рефлексии идём url=localhost:5002 (без http)
 builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
-
+app.MapGrpcReflectionService();
 app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "HelloWorld");
+//app.MapGet("/", () => "HelloWorld");
 
 app.Run();
